5933769d854c8a3884799584df995ee89896b6c4
MaulidaRiska <maulidariska190@gmail.com>
2021-04-09 16:58:09 -0700

    Operator Binary Assigment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah commenr/komentar yang tidak di jalankan oleh program
+
+    String nama = "Maulida Riska Nilam Sari";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}

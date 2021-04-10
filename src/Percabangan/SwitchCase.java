86cde8a99fdcac63e1886413a04424ad2c483398
MaulidaRiska <maulidariska190@gmail.com>
2021-04-09 18:57:28 -0700

    Switch Case

---
+++ src/Percabangan/SwitchCase.java
@@ -0,0 +1,25 @@
+package Percabangan;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        int MenuKue=4;
+        switch (MenuKue){
+            case 1:
+                System.out.println("Kue Cubit");
+                break;
+            case 2:
+                System.out.println("Kue Pisang");
+                break;
+            case 3:
+                System.out.println("kue Cucur");
+                break;
+            case 4:
+                System.out.println("Kue Bolu");
+                break;
+            default:
+                System.out.println("Kue Bingka");
+                break;
+
+        }
+    }
+}

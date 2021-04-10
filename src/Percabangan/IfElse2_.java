6bedf0700981592193567007ac83dbc6503c6e08
MaulidaRiska <maulidariska190@gmail.com>
2021-04-09 18:42:34 -0700

    If Else2

---
+++ src/Percabangan/IfElse2_.java
@@ -0,0 +1,16 @@
+package Percabangan;
+
+public class IfElse2_ {
+        public static void main(String[] args) {
+            double TotalBelanja = 542000;
+            double YangDiBayar = 531000;
+
+            if(YangDiBayar<TotalBelanja){
+                System.out.println("Uang Anda Kurang, Kurangi Belanjaan Anda");
+            }else {
+                System.out.println("Uang Anda Cukup Kakak Selamat Berbelanja Lagi");
+            }
+        }
+
+
+}
